0e4b540daccfcda060760de5077f0430aedf9b99
atiqisrak <atiq.israk@webable.digital>
2025-11-21 20:18:07 +0600

    retry 3

--- app/auth/profile/route.ts
+++ app/auth/profile/route.ts
@@ -1,15 +1,17 @@
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 import { NextRequest } from "next/server";
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
 
-const auth0 = new Auth0Client({
-  routes: {
-    callback: "/api/auth/callback",
-    login: "/api/auth/login",
-    logout: "/api/auth/logout",
-  },
-});
-
 export async function GET(req: NextRequest) {
+  const auth0 = new Auth0Client({
+    routes: {
+      callback: "/api/auth/callback",
+      login: "/api/auth/login",
+      logout: "/api/auth/logout",
+    },
+  });
   const authClient = (auth0 as any).authClient;
   return authClient.handleProfile(req);
 }
